d1d86abd04b1e2971590e73a38c0f2e9e69f0b71
chum2987 <chum2987@gmai.com>
2020-03-27 04:12:16 +0900

    done user management functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to user_path(current_user)
     else
       render :edit
     end
   end
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image, :text).merge(id: current_user.id)
   end
   
 end
