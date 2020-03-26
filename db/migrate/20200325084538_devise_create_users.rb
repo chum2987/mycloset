dd0eb23a5793db7a160dab796859783e510822eb
chum2987 <chum2987@gmail.com>
2020-03-27 03:08:05 +0900

    user table add image, text

--- db/migrate/20200325084538_devise_create_users.rb
+++ db/migrate/20200325084538_devise_create_users.rb
@@ -4,9 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null:false
+      t.text   :image
+      t.text   :text
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name,               null:false
 
       ## Recoverable
       t.string   :reset_password_token
