b25bc156af306cb2c88cb19aafb7e39ef85b8d1b
chum2987 <chum2987@gmail.com>
2020-03-27 00:13:54 +0900

    create my page

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,4 +1,5 @@
 class OutfitsController < ApplicationController
   def index
+   
   end
 end
--- db/migrate/20200325084844_create_outfits.rb
+++ db/migrate/20200325084844_create_outfits.rb
@@ -1,7 +1,9 @@
 class CreateOutfits < ActiveRecord::Migration[5.2]
   def change
     create_table :outfits do |t|
-
+      t.text :image, null: false
+      t.text :text
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
